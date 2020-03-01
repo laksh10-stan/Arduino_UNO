25c45d88d3cba12e5b1d25ba0ffe19388b052f6d
Lakshmendra Singh <43860070+Kushjadaun@users.noreply.github.com>
2020-03-01 19:10:14 +0530

    init commit

---
+++ Blink/Blink.ino
@@ -0,0 +1,16 @@
+int data=0;
+void setup() {
+  Serial.begin(9600);
+  pinMode(7, OUTPUT);
+  pinMode(6, OUTPUT);
+   pinMode(9, OUTPUT);
+  pinMode(8, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(7, HIGH);
+   digitalWrite(6, LOW);   
+ 
+     
+                       
+}
